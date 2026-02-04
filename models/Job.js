0149184ecffbf056c1b37a008f5044636bd5b796
JorgeMtzIs <martinezislasjorge1@gmail.com>
2026-02-04 17:58:13 -0600

    week 9 corrections

--- models/Job.js
+++ models/Job.js
@@ -1,30 +1,29 @@
-const { required } = require("joi");
-const mongooose = require("mongoose");
+const mongoose = require("mongoose");
 
-const JobSchema = new mongooose.Schema(
+const JobSchema = new mongoose.Schema(
   {
     company: {
       type: String,
       required: [true, "Please provide company length"],
-      maxlength: 50,
+      maxlength: 50
     },
     position: {
       type: String,
       required: [true, "Please provide position"],
-      maxlength: 200,
+      maxlength: 200
     },
     status: {
       type: String,
-      enum: ["interview", "declinde", "pending"],
-      default: "pending",
+      enum: ["interview", "declined", "pending"],
+      default: "pending"
     },
     createdBy: {
-      type: mongooose.Types.ObjectId,
+      type: mongoose.Types.ObjectId,
       ref: "User",
-      required: [true, "Please provide user"],
-    },
+      required: [true, "Please provide user"]
+    }
   },
-  { timestamps: true },
+  { timestamps: true }
 );
 
-module.exports = mongooose.model("Job", JobSchema);
+module.exports = mongoose.model("Job", JobSchema);
--- models/User.js
+++ models/User.js
@@ -1,4 +1,3 @@
-const { required } = require("joi");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -8,7 +7,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide name"],
     minlength: 3,
-    maxlength: 50,
+    maxlength: 50
   },
   email: {
     type: String,
@@ -16,15 +15,15 @@ const UserSchema = new mongoose.Schema({
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       ,
-      "Please provide valid email",
+      "Please provide valid email"
     ],
-    unique: true,
+    unique: true
   },
   password: {
     type: String,
     required: [true, "Please provide password"],
-    minlength: 6,
-  },
+    minlength: 6
+  }
 });
 
 UserSchema.pre("save", async function (next) {
@@ -37,7 +36,7 @@ UserSchema.methods.createJWT = function () {
   return jwt.sign(
     { userId: this._id, name: this.name },
     process.env.JWT_SECRET,
-    { expiresIn: process.env.JWT_LIFETIME },
+    { expiresIn: process.env.JWT_LIFETIME }
   );
 };
 
